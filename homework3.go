a0ec95dc111bc0678e046668301a225704ac9df6
Furkan Taşkın <109654632+furykan@users.noreply.github.com>
2022-08-24 12:44:13 +0300

    Homework3 Finished

---
+++ homework3.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var weight float32 = 165
+	fmt.Println("Your current weight : ", weight)
+	var desiredWeight float32 = 105.0
+	fmt.Println("The weight you want to reach :", desiredWeight)
+	fmt.Println("*****************")
+	var dailyFatPercentage float32 = 1.5
+
+	//aşağıdaki fonksiyondan dönen değer ekrana
+	//kaybedilen kilo ve şuan ulaşmış olunan kilo olarak yazılacaktır
+	weightLost := weightLoss(&weight, desiredWeight, dailyFatPercentage)
+	fmt.Println("Your new weight : ", weight)
+	fmt.Println("Your lost weight : ", weightLost)
+
+	switch {
+	case weight > desiredWeight:
+		fmt.Println("Sports should be continued")
+	case weight == desiredWeight:
+		fmt.Println("the desired weight has been reached")
+	case weight < desiredWeight:
+		fmt.Println("lost more weight than desired")
+	default:
+		fmt.Println("no measurement done")
+
+	}
+
+}
+
+//kilo ölçümü yapılacak kişinin spora başlamadan önce ki kilosu ve
+//bir günde yakacağı yağ oranı mevcut kilosundan çıkarılarak
+//günlük bir döngüyle mevcut kilosundan düşecektir.
+
+func weightLoss(weight *float32, desiredWeight float32, dailyFatPercentage float32) float32 {
+	startingWeight := *weight
+	for *weight > desiredWeight {
+		fmt.Println(*weight)
+		*weight -= dailyFatPercentage
+
+	}
+
+	return startingWeight - *weight
+}
